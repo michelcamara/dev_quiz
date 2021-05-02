d04bdfa5b1f65065f0e2106113aab41b6519430b
Michel Câmara <michelvictor16@gmail.com>
2021-05-02 14:13:41 -0400

    Criação dos models
    Answer, questin, quiz, user

---
+++ lib/shared/widgets/model/user_model.dart
@@ -0,0 +1,12 @@
+class UserModel {
+  final String name;
+  final String photoUrl;
+  final int score;
+  UserModel({
+    required this.name,
+    required this.photoUrl,
+    required this.score,
+  });
+}
+
+//Continuar de: Aula 03 - 22:36min
